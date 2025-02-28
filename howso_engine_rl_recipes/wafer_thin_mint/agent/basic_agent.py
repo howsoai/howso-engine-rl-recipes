bea4d50441b628145e3816b5d7b0c1c8a34f6ad6
howsoRes <144272317+howsoRes@users.noreply.github.com>
2025-02-28 15:00:04 -0500

    removed use case weights

--- howso_engine_rl_recipes/wafer_thin_mint/agent/basic_agent.py
+++ howso_engine_rl_recipes/wafer_thin_mint/agent/basic_agent.py
@@ -85,8 +85,7 @@ class BasicAgent(BaseAgent[int, int]):
             action_features=self.action_features,
             goal_features_map=self.goal_features_map,
             into_series_store=str(round_num),
-            details=details,
-            use_case_weights=True
+            details=details
         )
         action = react['action']['action'][0]
 
